2e6c0b7952d6d8e77929b909db8720955dc96921
Muhammad Asim <mrgoblin@Muhammads-MacBook-Pro.local>
2023-08-20 14:03:05 +0500

    self signed certificate generated and https server established

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const path = require('path')
 const https = require('https')
 const express = require('express')
@@ -7,9 +8,19 @@ const PORT = 3000 || env.PORT
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    console.log('hello')
+  return res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`)
+app.get('/secret', (req, res) => {
+  return res.send('Your personal secret value is 8080')
 })
+
+https
+  .createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+  }, app)
+  .listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`)
+  })
